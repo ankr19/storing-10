f244b3f65f5834ef584e169640699428f048b094
ankm19 <ankm335@gmail.com>
2022-08-22 21:42:12 +0530

    new

---
+++ routes/rddata.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const dbmodel = require('../models/dbmodel')
+
+const router = express.Router();
+
+// route 1
+// storing the db data through the POST 
+router.post("/dbdata", async(req, res)=>{
+
+})

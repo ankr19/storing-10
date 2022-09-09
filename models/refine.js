d7311297f2acce197b0bbf777386fd4df975bcea
ankm19 <ankm335@gmail.com>
2022-09-09 13:04:11 +0530

    autoincrement

--- models/refine.js
+++ models/refine.js
@@ -1,9 +1,12 @@
 const mongoose = require('mongoose');
+var AutoIncrement = require('mongoose-sequence')(mongoose);
 const {Schema} = mongoose;
 const MainModel = new Schema({
+    ID: Number,
     data: [Object],
 });
 
+MainModel.plugin(AutoIncrement, {inc_field: 'ID'})
 const refine = mongoose.model('MainData', MainModel);
 
 module.exports = refine;
